6d980100ec85a806e963a2a27847831040345dd2
Beniamin Cioban <beniamin.cioban@student.usv.ro>
2023-10-27 18:33:43 +0300

    Adaugare Camera

---
+++ Camera.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+
+namespace CIOBAN
+{
+    public class Camera
+    {
+        public Vector3 position { get; set; }
+        private Vector3 direction;
+
+
+        private float _zNear;
+        private float _zFar;
+        public float zNear {
+            get { return _zNear; }
+            set { _zNear = (value <= 0) ? 0.1f : (value>=_zFar)? 0.1f: value; }
+        }
+        public float zFar {
+            get { return _zFar; }
+            set { _zFar = (value <= _zNear) ? _zNear + 1f :value; }
+        }
+
+        public Vector3 Forward { get { return new Vector3((float) Math.Sin(direction.X), 0, (float)Math.Cos(direction.X)); } }
+        public Vector3 Right { get { return new Vector3(-Forward.Z, 0, Forward.X); } }
+
+        public Camera(Vector3 Position)
+        {
+            _zNear = 0.1f;
+            _zFar = 1000f;
+            position = Position;
+            direction = new Vector3((float)Math.PI, 0f, 0f);
+        }
+
+        public Matrix4 GetMatrix()
+        {
+            Vector3 lookat = new Vector3((float)(Math.Sin(direction.X) * Math.Cos(direction.Y)),
+                                            (float)Math.Sin(direction.Y),
+                                            (float)(Math.Cos(direction.X) * Math.Cos(direction.Y)));
+            return Matrix4.LookAt(position, position + lookat, Vector3.UnitY);
+        }
+        public void MoveCamera(Vector3 Direction)
+        {
+
+            Vector3 offset = Vector3.Zero;
+
+            offset += Direction.X * Right;
+            offset += Direction.Y * Forward;
+            offset.Y += Direction.Z;
+
+            offset.NormalizeFast();
+
+            position += offset;
+        }
+        public void AddRotation(float x, float y)
+        {
+            direction.X = (direction.X + x) % ((float)Math.PI * 2.0f);
+            direction.Y = Math.Max(Math.Min(direction.Y + y, (float)Math.PI / 2.0f - 0.1f), (float)-Math.PI / 2.0f + 0.1f);
+        }
+        public void UpdateCamera()
+        {
+            Matrix4 lookat = GetMatrix();
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+        }
+        public void ChangePerspectiveFieldOfView(float aspect_ratio)
+        {
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, _zNear, _zFar);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,31 +18,62 @@ namespace CIOBAN
         //Laboratorul 2
         #region L2
         private KeyboardState lastFrameKeyboard;
-        Key moveTriangleKey = Key.Q;
+        private Key moveTriangleKey = Key.Q;
         private bool moveTriangle = true;
-        Key renderTriangleKey = Key.E;
+        private Key renderTriangleKey = Key.E;
         private bool renderTriangle = true;
 
         Vector3 poz = new Vector3();
+        // zPoz folosit pentru scalarea tiunghiului folosind rotita
         private float zPoz = 0;
+
         private float mouseSensitivity = 0.1f;
 
         #endregion
         #region L3-4
-        Vector3 cameraPosition = new Vector3(5,5,5);
-        private float movementSpeed = .5f;
-        Key cameraForwardKey = Key.W;
-        Key cameraBackwardsKey = Key.S;
-        Key cameraLeftKey = Key.A;
-        Key cameraRightKey = Key.D;
-        Key cameraUpKey = Key.LShift;
-        Key cameraDownKey = Key.LControl;
+        /// <summary>
+        /// Folosesc clasa creata "Camera"
+        /// pentru a putea controla view-ul
+        /// </summary>
+        public Camera camera;
+        // rotX si rotY sunt folosite pentru a calcula rotatia camerei
+        // folosind datele de la mouse .X si .Y
+        private float rotX = 0;
+        private float rotY = 0;
+        // "movementSpeed" si "mouseSensitivity" (din L2)
+        // determina sensivitatea controlului camerei
+        private float movementSpeed = 2f;
+        // Un set de key pentru determinarea inputului
+        // care va misca camera
+        private readonly Key cameraForwardKey = Key.W;
+        private readonly Key cameraBackwardsKey = Key.S;
+        private readonly Key cameraLeftKey = Key.A;
+        private readonly Key cameraRightKey = Key.D;
+        private readonly Key cameraUpKey = Key.LShift;
+        private readonly Key cameraDownKey = Key.LControl;
+        private readonly Key lockCameraKey = Key.Number1;
+        // "lockCamera" blocheaza controlul camerei
+        // daca este setat pe True
+        private bool lockCamera = false;
+
+
+        // Un set de key pentru schimbarea 
+        // colorilor triunghiului randat
+        private readonly Key change1Key = Key.Z;
+        private Color color1 = Color.White;
+        private readonly Key change2Key = Key.X;
+        private Color color2 = Color.White;
+        private readonly Key change3Key = Key.C;
+        private Color color3 = Color.White;
 
         #endregion
 
         public Program() : base(800, 600)
         {
             KeyDown += WindowSettings;
+            // L3 
+            // Initializeaza camera si o pozitie aleatorie
+            camera = new Camera(new Vector3(5,5,-5));
         }
         // Functie in care prelucreaza inputul pentru modificarea ferestrei
         void WindowSettings(object sender, KeyboardKeyEventArgs e)
@@ -53,11 +84,7 @@ namespace CIOBAN
             if (e.Key == Key.F11)
                 this.WindowState = (this.WindowState == WindowState.Fullscreen)? WindowState.Normal : WindowState.Fullscreen;
         }
-        public Color GetRandomColor()
-        {
-            Random rnd = new Random();
-            return Color.FromArgb(rnd.Next(0,256), rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
-        }
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -69,7 +96,7 @@ namespace CIOBAN
         protected override void OnResize(EventArgs e)
         {
             base.OnLoad(e);
-            /*
+            /* L2
              * Vizualizare 2d (X,Y)
             GL.Viewport(0, 0, Width, Height);
             GL.MatrixMode(MatrixMode.Modelview);
@@ -80,16 +107,18 @@ namespace CIOBAN
             GL.Viewport(0, 0, Width, Height);
 
             double aspect_ratio = Width / (double)Height;
-
-            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 0.1f, 1000);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadMatrix(ref perspective);
-
-            Matrix4 lookat = Matrix4.LookAt(cameraPosition, new Vector3(0, 0, 0), new Vector3(0, 1f, 0));
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref lookat);
+            // L3-4 Seteaza perspectiva si camera
+            camera.ChangePerspectiveFieldOfView((float)aspect_ratio);
+            camera.UpdateCamera();
         }
-
+        
+        // L3
+        // Functie pentru generarea unei culor aleatorie
+        public Color GetRandomColor()
+        {
+            Random rnd = new Random();
+            return Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+        }
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
@@ -104,30 +133,66 @@ namespace CIOBAN
         {
             KeyboardState keyboard = Keyboard.GetState();
             MouseState mouse = Mouse.GetState();
-            if(moveTriangle)
-            {
-                float delta = 1 / 30f;
-                poz.X = mouse.X * delta * mouseSensitivity;
-                poz.Y = -mouse.Y * delta * mouseSensitivity;
 
-                if(zPoz - mouse.WheelPrecise > 0)
-                {
-                    poz.Z -= delta;
-                }
-                else if(zPoz - mouse.WheelPrecise < 0)
-                    poz.Z += delta;
-                if (poz.Z == 0)
-                    poz.Z = .1f;
-                zPoz = mouse.WheelPrecise;
-            }
             // Prelucreaza variabilele boolene pentru a
             // lucra doar la o singura apasare a tasteii
             moveTriangle = (keyboard.IsKeyDown(moveTriangleKey) && lastFrameKeyboard.IsKeyUp(moveTriangleKey)) ? !moveTriangle : moveTriangle;
             renderTriangle = (keyboard.IsKeyDown(renderTriangleKey) && lastFrameKeyboard.IsKeyUp(renderTriangleKey)) ? !renderTriangle : renderTriangle;
 
-            cameraPosition.X += (keyboard.IsKeyDown(cameraLeftKey) ? (-1f) : keyboard.IsKeyDown(cameraRightKey) ? (1f) : 0) * deltaTime;
-            cameraPosition.Y += (keyboard.IsKeyDown(cameraDownKey) ? (-1f) : keyboard.IsKeyDown(cameraUpKey) ? (1f) : 0) * deltaTime;
-            cameraPosition.Z += (keyboard.IsKeyDown(cameraBackwardsKey) ? (-1f) : keyboard.IsKeyDown(cameraForwardKey) ? (1f) : 0) * deltaTime;
+            // Misca triunghiul dupa inputul mouse-ului
+            if(moveTriangle)
+            {
+                poz.X = mouse.X  * mouseSensitivity;
+                poz.Y = -mouse.Y * mouseSensitivity;
+
+                if(zPoz - mouse.WheelPrecise > 0)
+                {
+                    poz.Z -= deltaTime;
+                }
+                else if(zPoz - mouse.WheelPrecise < 0)
+                    poz.Z += deltaTime;
+                if (poz.Z == 0)
+                    poz.Z = .1f;
+                zPoz = mouse.WheelPrecise;
+            }
+
+            // L3
+            // Prelucreaza inputul pentru a seta culori noi vertexurilor triunghiului
+            if (keyboard.IsKeyDown(change1Key) && lastFrameKeyboard.IsKeyUp(change1Key))
+                color1 = GetRandomColor();
+            if (keyboard.IsKeyDown(change2Key) && lastFrameKeyboard.IsKeyUp(change2Key))
+                color2 = GetRandomColor();
+            if (keyboard.IsKeyDown(change3Key) && lastFrameKeyboard.IsKeyUp(change3Key))
+                color3 = GetRandomColor();
+
+            // L3
+            // Blocheaza sau nu controlul camerei
+            lockCamera = (keyboard.IsKeyDown(lockCameraKey) && lastFrameKeyboard.IsKeyUp(lockCameraKey)) ? !lockCamera : lockCamera;
+
+            // L3
+            // Prelucreaza inputul pentru a misca camera
+            if(!lockCamera)
+            {
+                // cameraPosition este pozitia camerei
+                // Reprezinta un vector3 (este vizualizat ca un vector local
+                // adica z+ fata, z- spate,
+                // y+ sus, y- jos,
+                // x- stanga, x+ dreapta)
+                Vector3 cameraPosition = camera.position;
+                cameraPosition.X = (keyboard.IsKeyDown(cameraLeftKey) ? (-1f) : keyboard.IsKeyDown(cameraRightKey) ? (1f) : 0) * deltaTime * movementSpeed;
+                cameraPosition.Z = (keyboard.IsKeyDown(cameraDownKey) ? (-1f) : keyboard.IsKeyDown(cameraUpKey) ? (1f) : 0) * deltaTime * movementSpeed;
+                cameraPosition.Y = (keyboard.IsKeyDown(cameraBackwardsKey) ? (-1f) : keyboard.IsKeyDown(cameraForwardKey) ? (1f) : 0) * deltaTime * movementSpeed;
+
+                // Apeleaza metodele de miscare a pozitiei si a rotatiei
+                // prelucreaza vectorul local pentru a aplica directiei sensului camerei
+                camera.MoveCamera(cameraPosition);
+                // calculeaza discrepanta dintre miscarea mouse-ului in functie de timpul parcurs in frame
+                camera.AddRotation((rotX - mouse.X) * deltaTime * mouseSensitivity, -(mouse.Y - rotY) * deltaTime * mouseSensitivity);
+            }
+            // L3
+            // Updateaza rotatiile precedente cu a mouse-ului
+            rotX = mouse.X;
+            rotY = mouse.Y;
 
             lastFrameKeyboard = keyboard;
         }
@@ -137,7 +202,11 @@ namespace CIOBAN
             base.OnRenderFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
-
+            // L3
+            // Modifica pozitia camerei
+            // Randeaza axele de coordonate
+            // Randeaza un grid
+            camera.UpdateCamera();
             DrawAxes();
             DrawGrid(Color.White,16);
             // L2
@@ -147,11 +216,6 @@ namespace CIOBAN
             {
                 DrawTriangle(poz.X, poz.Y, poz.Z);
             }
-            // L3
-            // Modifica pozitia camerei
-            Matrix4 lookat = Matrix4.LookAt(cameraPosition, new Vector3(0, 0, 0), new Vector3(0, 1f, 0));
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref lookat);
 
             this.SwapBuffers();
         }
@@ -163,11 +227,11 @@ namespace CIOBAN
         public void DrawTriangle(float x, float y, float z)
         {
             GL.Begin(PrimitiveType.Triangles);
-            GL.Color3(Color.Red);
+            GL.Color3(color1);
             GL.Vertex3(x - z, y + z, 0);
-            GL.Color3(Color.Green);
+            GL.Color3(color2);
             GL.Vertex3(x, y - z, 0);
-            GL.Color3(Color.Blue);
+            GL.Color3(color3);
             GL.Vertex3(x + z, y + z, 0);
             GL.End();
         }
@@ -175,30 +239,33 @@ namespace CIOBAN
         // Afiseaza axele de coordonate 
         public void DrawAxes()
         {
-            GL.LineWidth(1);
+            GL.LineWidth(5);
             GL.Begin(PrimitiveType.Lines);
             GL.Color3(Color.Red);
             GL.Vertex3(0, 0, 0);
-            GL.Vertex3(1, 0, 0);
+            GL.Vertex3(2, 0, 0);
             GL.Color3(Color.Green);
             GL.Vertex3(0, 0, 0);
-            GL.Vertex3(0, 1, 0);
+            GL.Vertex3(0, 2, 0);
             GL.Color3(Color.Blue);
             GL.Vertex3(0, 0, 0);
-            GL.Vertex3(0, 0, 1);
+            GL.Vertex3(0, 0, 2);
             GL.End();
             GL.LineWidth(0.1f);
         }
+        // L3
+        // Randeaza un grid cu o anumita
+        // culoare si un nr de linii
         public void DrawGrid(Color gridColor,int halfSize)
         {
             GL.Begin(PrimitiveType.Lines);
             GL.Color3(gridColor);
-            for (int i=-halfSize;i<=halfSize;i++)
+            for (int i = -halfSize; i <= halfSize; i++)
             {
-                GL.Vertex3(i,0,-halfSize);
-                GL.Vertex3(i,0,halfSize);
+                GL.Vertex3(i, 0, -halfSize);
+                GL.Vertex3(i, 0, halfSize);
 
-                GL.Vertex3(-halfSize,0,-i);
+                GL.Vertex3(-halfSize, 0, -i);
                 GL.Vertex3(halfSize, 0, -i);
             }
             GL.End();
